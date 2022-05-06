fd5d822f612ce34320c2553b4e3bceb4e7297fe4
marlonhanks <owner@kedia.co>
2022-05-06 08:16:29 +0000

    Deploying to gh-pages from @ litemaker/plasm-apps-vue@bc8c972128067f38dac0b50875ad4f260659bacd 🚀

---
+++ precache-manifest.2ca7bac381cc98dcd9055e8660db42e2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c9e4f2880050b5fd35",
+    "url": "/css/app.bd19371a.css"
+  },
+  {
+    "revision": "671566427e8c235975c917d8bcbd2d6d",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "96bf6b5920598d6c80c0a6dc41b46c70",
+    "url": "/img/ACA.96bf6b59.96bf6b59.svg"
+  },
+  {
+    "revision": "b78d1f7d17f6f87386fcf33c6993c9d3",
+    "url": "/img/astar.b78d1f7d.png"
+  },
+  {
+    "revision": "b64767e32cf8d74a4799ebf6e29e9c30",
+    "url": "/img/dusty.b64767e3.png"
+  },
+  {
+    "revision": "ef90c06094546061fa3bfaf5a2bfd31a",
+    "url": "/img/logo-chainlink.ef90c060.png"
+  },
+  {
+    "revision": "c09f7195a93c6db06819a5cb9a4f65b1",
+    "url": "/img/logo-metamask.c09f7195.png"
+  },
+  {
+    "revision": "1ed473b4edfba5629ae725a1fb6e3c75",
+    "url": "/img/logo-polkadot-js.1ed473b4.png"
+  },
+  {
+    "revision": "87195b063d39d8316dc39ba33b2f1292",
+    "url": "/img/logo-subscan.87195b06.png"
+  },
+  {
+    "revision": "e814f1e5c830f4e5aa29e3c4d78b87fc",
+    "url": "/img/plasm.e814f1e5.png"
+  },
+  {
+    "revision": "95e08eaec55b32a320e4884db044eb21",
+    "url": "/img/shiden.png"
+  },
+  {
+    "revision": "1f2cf5b4632e4dfb59537725c5a5e14f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c9e4f2880050b5fd35",
+    "url": "/js/app.e76bdae9.js"
+  },
+  {
+    "revision": "df50bcd6a4b95b55ce86",
+    "url": "/js/chunk-vendors.079f44ce.js"
+  },
+  {
+    "revision": "fd22b319c20d42b56707d53228e2d3b1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
